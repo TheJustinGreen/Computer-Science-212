4b9e0f958245e2bfd9a8401bf5870c07d55438da
TheJustinGreen <greenjustin2016@gmail.com>
2016-09-02 14:29:29 +0200

    Dynamic Fib

---
+++ DPFib.java
@@ -0,0 +1,30 @@
+public class DPFib{
+
+	/*
+		 * Recursive Fibonacci Algorithm Complexity
+		 * 
+		 * Time Complexity: O(n)
+		 * Extra Space: O(n)
+		 * 
+		 * This is good since it is the time is proportional the data size
+		 * 
+		 */
+		
+		public int dpFibCalc(int n)
+		{	
+			int[] fib = new int[n+1];
+			
+			for(int k = 1; k < n+1; k++)
+				fib[k] = (k <= 2) ? 1 : fib[k-1] + fib[k-2];
+			
+			return fib[n];
+		}
+		
+		public static void main(String[] args)
+		{
+			DPFib run = new DPFib();
+			//Insert the number for the fib term you wish to calculate) 
+			System.out.println(run.dpFibCalc(7));
+		}
+}
+
