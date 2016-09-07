95bcefb3c9a48c99201f937bae47b6c75bab67b7
TheJustinGreen <greenjustin2016@gmail.com>
2016-09-07 16:24:15 +0200

    edit version

---
+++ fibRecursiveMemo.java
@@ -0,0 +1,21 @@
+/*
+ *Recursive Fibonacci with memoization
+ *
+ * The time complexity of this algorithm is 0(n)
+ * since recusrsive calls count as O(1) with memoization
+ * 
+ * 
+ */
+public class fibRecursiveMemo {
+
+	//Random size of array//
+	int[] F = new int[999];
+	
+	public int fibCalc(int n){ return (n < 3) ? 1 : (F[n] == 0) ? F[n] = fibCalc(n-1) + fibCalc(n-2) : F[n]; }
+	
+	public static void main(String[] args)
+	{
+		fibRecursiveMemo run = new fibRecursiveMemo();
+		System.out.println(run.fibCalc(6));
+	}
+}
